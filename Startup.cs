37a4efd0122b9acb5c3f8bd0f9325f07c9662708
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2018-10-31 00:15:11 -0300

    Initial Commit

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,6 @@ namespace CampanhaInfopharma
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            
         }
 
         public IConfiguration Configuration { get; }
