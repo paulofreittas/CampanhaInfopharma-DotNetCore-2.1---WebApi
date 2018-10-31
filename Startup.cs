80cab5316bfc428c022d4d254f542df617cfe4e1
Paulo Henrique Freitas <paulo-henriquee@hotmail.com>
2018-10-31 00:14:27 -0300

    Configuration test

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace CampanhaInfopharma
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
